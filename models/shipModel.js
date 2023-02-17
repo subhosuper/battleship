d6698c4881c31a06b16f3fbf9d9971fbd0b49f0b
Subham Garai <garaisubham34@gmail.com>
2023-02-17 21:55:57 +0530

    Adds ship model

---
+++ models/shipModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const shipSchema = new mongoose.Schema({
+    sessionId: {
+        type: String,
+        // ref: 'Session',
+        required: [true, "SessionRef is required"],
+    },
+    type: {
+        type: String,
+        required: [true, "Ship type shouldn't be empty"]
+    },
+    coordinates: {
+        type: Array,
+        required: [true, "Ship coordinates shouldn't be empty"]
+    }
+});
+
+const Ship = mongoose.model("Ship", shipSchema);
+
+module.exports = Ship;
