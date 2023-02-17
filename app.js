687566d1e0575a435fe65f24d5540f1e52fc65d3
Subham Garai <garaisubham34@gmail.com>
2023-02-17 15:36:30 +0530

    Adds statuts route to app

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const statusRoute = require("./routes/statusRoute");
 
 const app = express();
 
 app.use(express.json()); //Attaches body in JSON form to request
+
+
+
+
+app.use("/api/v1/status", statusRoute);
+
+
+
+
+
 
 module.exports = app;
