e3a932c2df41fff6281b238c8f534c61155a0f64
Subham Garai <garaisubham34@gmail.com>
2023-02-17 17:09:20 +0530

    Adds new game API

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+// import routes
 const statusRoute = require("./routes/statusRoute");
+const newGameRoute = require("./routes/newGameRoute.js")
 
 const app = express();
-
+// Middleware for express
 app.use(express.json()); //Attaches body in JSON form to request
 
 
+// APIs
+
+app.use("/api/v1/new-game", newGameRoute)
 
 
 app.use("/api/v1/status", statusRoute);
-
 
 
 
