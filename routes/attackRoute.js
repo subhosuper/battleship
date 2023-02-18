29b1a6f13610d2c7bbe76d6d21fcb715a410d385
Subham Garai <garaisubham34@gmail.com>
2023-02-19 04:11:28 +0530

    Adds validation middleware for attack

--- routes/attackRoute.js
+++ routes/attackRoute.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {attackController} = require("../controllers/attackController");
+const attackValidation = require("../utils/attackValidation");
 
 router.route("/")
-                .post(attackController)
+                .post(attackValidation.validateAttack, attackController)
 
 
 module.exports = router;
