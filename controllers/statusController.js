06531cc7249fca368fbfe1205e5274f7f5678f94
Subham Garai <garaisubham34@gmail.com>
2023-02-20 03:58:19 +0530

    Adds status controller, complete status API

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -1,12 +1,48 @@
-const {gameState, ground} = require("../utils/constants");
+const Ship = require("../models/shipModel");
+const missedMoves = require("../models/missedModel");
+const {constants} = require("../utils/constants");
+const attackValidations = require("../utils/attackValidation");
 
-exports.getGameStatus = (req, res) => {
-    // const resp = store.getGameState();
-    console.log(gameState)
+const getAllHits = async (sessionId) => {
+    const allHits = [];
+    const query = Ship.find({sessionId: sessionId, status: "hit"}, {_id: 0, coordinates: 1, status: 1})
+    const gameState = await query;
+
+    for (doc of gameState){
+        for (eachIndex in doc["status"]) {
+            if (doc["status"][eachIndex] === "hit"){
+                var indexOfHit = eachIndex;
+                allHits.push(doc["coordinates"][[indexOfHit]]);
+            }
+        }
+    }
+    return allHits;
+}
+
+const getAllMiss = async (sessionId) => {
+
+    const query = missedMoves.distinct("coordinates", {sessionId: sessionId});
+    const missedCoordinates = await query;
+    return missedCoordinates;
+} 
+
+exports.getGameStatus = async (req, res) => {
+
+    const sessionId = req.headers["sessionid"];
+    const allHits = await getAllHits(sessionId);
+    const allMissed = await getAllMiss(sessionId);
+    
+    const data = {
+        missed: allMissed,
+        hits: allHits,
+        gridX: constants.GRID_X,
+        gridY: constants.GRID_Y
+    }
+
     res
         .status(200)
         .json({
-            "data": ground,
-            "status": "successss"
+            data: data,
+            "status": "success"
         });
 }
