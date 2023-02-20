e26e1258d32bcf475e834516ac890a99988925be
Subham Garai <garaisubham34@gmail.com>
2023-02-20 06:14:12 +0530

    Remove unused constants

--- utils/constants.js
+++ utils/constants.js
@@ -20,36 +20,8 @@ const SHIP_SIZE = {
 Object.keys(shipTypeCounts).forEach(el => TOTAL_UNITS_OF_ALL_SHIPS += SHIP_SIZE[el]*shipTypeCounts[el]); 
 console.log("Total units of all ships: ", TOTAL_UNITS_OF_ALL_SHIPS)
 
-const ground = [
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            ]
 
 const HIT = {"color": "red", value: 1};
 const MISS = {"color": "blue", value: 2};
-
-const gameState = {
-    "battleship-1": {hits: 0, size: 4},
-    "cruiser-1": {hits: 0, size: 3},
-    "cruiser-2": {hits: 0, size: 3},
-    "destroyer-1": {hits: 0, size: 2},
-    "destroyer-2": {hits: 0, size: 2},
-    "destroyer-3": {hits: 0, size: 2},
-    "submarine-1": {hits: 0, size: 1},
-    "submarine-2": {hits: 0, size: 1},
-    "submarine-3": {hits: 0, size: 1},
-    "submarine-4": {hits: 0, size: 1},
-    "sunk": 0,
-    "miss": [], // store co-ordinates of the missed attacks
-    "hit": []   // store co-ordinates of the hit attacks 
-};
 
 exports.constants = {GRID_X, GRID_Y, TOTAL_SHIP_COUNT, shipTypeCounts, TOTAL_UNITS_OF_ALL_SHIPS, SHIP_SIZE};
