811ca1a8a9599cc1dc69eb2b20062831b81ffa5a
Subham Garai <garaisubham34@gmail.com>
2023-02-20 06:12:32 +0530

    Adds input validators as route middleware

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const newGameRoute = require("./routes/newGameRoute.js");
 const placeShipRoute = require("./routes/placeShipRoute");
 const attackRoute = require("./routes/attackRoute");
 
+// import input validation
+const {shipInputValidator} = require("./utils/shipValidation");
+const attackInputValidation = require("./utils/attackValidation");
+
 const app = express();
 // Middleware for express
 app.use(express.json()); //Attaches body in JSON form to request
@@ -17,9 +21,9 @@ app.use(express.json()); //Attaches body in JSON form to request
 // APIs
 app.use("/api/v1/new-game", newGameRoute) // Done
 
-app.use("/api/v1/place-ship", placeShipRoute) // Done - add horizontal direction and test isSetupDone
+app.use("/api/v1/place-ship", shipInputValidator, placeShipRoute) // Done - add horizontal direction and test isSetupDone
 
-app.use("/api/v1/attack", attackRoute)
+app.use("/api/v1/attack", attackInputValidation.attackInputValidator, attackRoute)
 
 app.use("/api/v1/status", statusRoute);
 
